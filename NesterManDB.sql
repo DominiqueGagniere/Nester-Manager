c7f7cb57be3de960541f3f9ec2bee8e73aa4e30b
Dominique Gagnière <dominique@gagnie.re>
2024-05-30 16:28:54 +0200

    xjdj

--- NesterManDB.sql
+++ NesterManDB.sql
@@ -5,44 +5,42 @@ CREATE ROLE mspr WITH LOGIN PASSWORD 'MSPR';
 CREATE SCHEMA IF NOT EXISTS "NesterManDB" AUTHORIZATION mspr;
 
 -- -----------------------------------------------------
--- Table `NesterManDB`.`instance`
+-- Table instance
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS "NesterManDB"."instance" (
-  "idAll_Instance" INT NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY ("idAll_Instance"))
-ENGINE = InnoDB;
-
+  idAll_Instance SERIAL PRIMARY KEY
+);
 
 -- -----------------------------------------------------
--- Table `NesterManDB`.`harvester`
+-- Table harvester
 -- -----------------------------------------------------
-CREATE TABLE "NesterManDB"."harvester" (
-    hostname VARCHAR(45) NOT NULL,
-    mac_addr VARCHAR(17) NOT NULL,
-    ip_lan VARCHAR(45) NOT NULL,
-    ip_wan VARCHAR(45) NOT NULL,
-    serialnum INT NOT NULL,
-    cpu_name VARCHAR(45) NOT NULL,
-    ram_size_gb INT NOT NULL,
-    disk_type VARCHAR(45) NOT NULL,
-    disk_size_gb INT NOT NULL,
-    os_version VARCHAR(15) NOT NULL,
-    harvester_version VARCHAR(15) NOT NULL
+CREATE TABLE IF NOT EXISTS "NesterManDB"."harvester" (
+  hostname VARCHAR(45) NOT NULL,
+  mac_addr VARCHAR(17) NOT NULL,
+  ip_lan VARCHAR(45) NOT NULL,
+  ip_wan VARCHAR(45) NOT NULL,
+  serialnum INT NOT NULL,
+  cpu_name VARCHAR(45) NOT NULL,
+  ram_size_gb INT NOT NULL,
+  disk_type VARCHAR(45) NOT NULL,
+  disk_size_gb INT NOT NULL,
+  os_version VARCHAR(15) NOT NULL,
+  harvester_version VARCHAR(15) NOT NULL
 ) INHERITS ("NesterManDB"."instance");
 
 -- -----------------------------------------------------
--- Table `NesterManDB`.`nester_server`
+-- Table nester_server
 -- -----------------------------------------------------
-CREATE TABLE "NesterManDB"."nester_server" (
-    hostname VARCHAR(45) NOT NULL,
-    mac_addr VARCHAR(17) NOT NULL,
-    ip_lan VARCHAR(45) NOT NULL,
-    ip_wan VARCHAR(45) NOT NULL,
-    serialnum INT NOT NULL,
-    cpu_name VARCHAR(45) NOT NULL,
-    ram_size_gb INT NOT NULL,
-    disk_type VARCHAR(45) NOT NULL,
-    disk_size_gb INT NOT NULL,
-    os_version VARCHAR(15) NOT NULL,
-    harvester_version VARCHAR(15) NOT NULL
+CREATE TABLE IF NOT EXISTS "NesterManDB"."nester_server" (
+  hostname VARCHAR(45) NOT NULL,
+  mac_addr VARCHAR(17) NOT NULL,
+  ip_lan VARCHAR(45) NOT NULL,
+  ip_wan VARCHAR(45) NOT NULL,
+  serialnum INT NOT NULL,
+  cpu_name VARCHAR(45) NOT NULL,
+  ram_size_gb INT NOT NULL,
+  disk_type VARCHAR(45) NOT NULL,
+  disk_size_gb INT NOT NULL,
+  os_version VARCHAR(15) NOT NULL,
+  harvester_version VARCHAR(15) NOT NULL
 ) INHERITS ("NesterManDB"."instance");
