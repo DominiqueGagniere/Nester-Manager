0a8c5a0d4a393610938c0d9a88301e349617ae6e
Dominique Gagnière <dominique@gagnie.re>
2024-05-30 16:31:51 +0200

    kkl

--- populateDB.sql
+++ populateDB.sql
@@ -15,27 +15,3 @@ BEGIN
             (format('host_server_%s', i), format('00:1A:2B:3C:4D:%02s', i), format('192.168.1.%s', i), format('10.0.1.%s', i), i + 10, 'Intel Xeon', 32, 'HDD', 1024, 'CentOS 7', '2.0', i + 10);
     END LOOP;
 END $$;
-
-
--- Ajout de données dans la table state_instance
-INSERT INTO "NesterManDB"."state_instance" (All_Instance_idAll_Instance, state) VALUES
-(1, 'ok'),
-(2, 'nok'),
-(3, 'decommissioning'),
-(4, 'stop'),
-(5, 'ok'),
-(6, 'nok'),
-(7, 'decommissioning'),
-(8, 'stop'),
-(9, 'ok'),
-(10, 'nok'),
-(11, 'stop'),
-(12, 'stop'),
-(13, 'stop'),
-(14, 'stop'),
-(15, 'ok'),
-(16, 'stop'),
-(17, 'ok'),
-(18, 'decommissioning'),
-(19, 'ok'),
-(20, 'ok');
