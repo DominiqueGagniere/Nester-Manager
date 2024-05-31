f1adf75a6a659143c07ce519e0dd40b09aaf4b33
Dominique Gagnière <dominique@gagnie.re>
2024-05-31 15:04:32 +0200

    fix id instance

--- populateDB.sql
+++ populateDB.sql
@@ -5,12 +5,12 @@ DECLARE
 BEGIN
     FOR i IN 1..10 LOOP
         INSERT INTO "NesterManDB"."harvester" 
-            (hostname, mac_addr, ip_lan, ip_wan, serialnum, cpu_name, ram_size_gb, disk_type, disk_size_gb, os_version, harvester_version, idAll_Instance)
+            (hostname, mac_addr, ip_lan, ip_wan, serialnum, cpu_name, ram_size_gb, disk_type, disk_size_gb, os_version, harvester_version, id_instance)
         VALUES
             (format('host_harvester_%s', i), format('00:1A:2B:3C:4D:%02s', i), format('192.168.0.%s', i), format('10.0.0.%s', i), i, 'Intel i7', 16, 'SSD', 512, 'Ubuntu 20.04', '1.0', i);
         
         INSERT INTO "NesterManDB"."nester_server" 
-            (hostname, mac_addr, ip_lan, ip_wan, serialnum, cpu_name, ram_size_gb, disk_type, disk_size_gb, os_version, nester_version, idAll_Instance)
+            (hostname, mac_addr, ip_lan, ip_wan, serialnum, cpu_name, ram_size_gb, disk_type, disk_size_gb, os_version, nester_version, id_instance)
         VALUES
             (format('host_server_%s', i), format('00:1A:2B:3C:4D:%02s', i), format('192.168.1.%s', i), format('10.0.1.%s', i), i + 10, 'Intel Xeon', 32, 'HDD', 1024, 'CentOS 7', '2.0', i + 10);
     END LOOP;
