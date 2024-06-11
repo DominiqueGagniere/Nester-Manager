60195a74f1b77949f0e9ffefe04496f034e93534
Dominique Gagnière <dominique@gagnie.re>
2024-06-11 19:25:53 +0200

    fix only instance

--- NesterManDB.sql
+++ NesterManDB.sql
@@ -22,40 +22,6 @@ CREATE TABLE IF NOT EXISTS NesterManDB.instance (
   nester_version VARCHAR(15)
 );
 
--- -----------------------------------------------------
--- Table harvester
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS NesterManDB.harvester (
-  hostname VARCHAR(45) NOT NULL,
-  mac_addr VARCHAR(17) NOT NULL,
-  ip_lan VARCHAR(45) NOT NULL,
-  ip_wan VARCHAR(45) NOT NULL,
-  serialnum INT NOT NULL,
-  cpu_name VARCHAR(45) NOT NULL,
-  ram_size_gb INT NOT NULL,
-  disk_type VARCHAR(45) NOT NULL,
-  disk_size_gb INT NOT NULL,
-  os_version VARCHAR(15) NOT NULL,
-  harvester_version VARCHAR(15) NOT NULL
-) INHERITS (NesterManDB.instance);
-
--- -----------------------------------------------------
--- Table nester_server
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS NesterManDB.nester_server (
-  hostname VARCHAR(45) NOT NULL,
-  mac_addr VARCHAR(17) NOT NULL,
-  ip_lan VARCHAR(45) NOT NULL,
-  ip_wan VARCHAR(45) NOT NULL,
-  serialnum INT NOT NULL,
-  cpu_name VARCHAR(45) NOT NULL,
-  ram_size_gb INT NOT NULL,
-  disk_type VARCHAR(45) NOT NULL,
-  disk_size_gb INT NOT NULL,
-  os_version VARCHAR(15) NOT NULL,
-  nester_version VARCHAR(15) NOT NULL
-) INHERITS (NesterManDB.instance);
-
 CREATE TABLE IF NOT EXISTS NesterManDB.client (
   id_client INT PRIMARY KEY,
   company_name VARCHAR(45) NOT NULL,
--- populateDB.sql
+++ populateDB.sql
@@ -3,12 +3,12 @@ DECLARE
     i INT;
 BEGIN
     FOR i IN 1..10 LOOP
-        INSERT INTO NesterManDB.harvester 
+        INSERT INTO NesterManDB.instance 
             (hostname, mac_addr, ip_lan, ip_wan, serialnum, cpu_name, ram_size_gb, disk_type, disk_size_gb, os_version, harvester_version)
         VALUES
             (format('host_harvester_%s', i), format('00:1A:2B:3C:4D:%02s', i), format('192.168.0.%s', i), format('10.0.0.%s', i), i, 'Intel i7', 16, 'SSD', 512, 'Ubuntu 20.04', '1.0');
         
-        INSERT INTO NesterManDB.nester_server 
+        INSERT INTO NesterManDB.instance 
             (hostname, mac_addr, ip_lan, ip_wan, serialnum, cpu_name, ram_size_gb, disk_type, disk_size_gb, os_version, nester_version)
         VALUES
             (format('host_server_%s', i), format('00:1A:2B:3C:4D:%02s', i), format('192.168.1.%s', i), format('10.0.1.%s', i), i + 10, 'Intel Xeon', 32, 'HDD', 1024, 'CentOS 7', '2.0');
@@ -20,11 +20,11 @@ VALUES
 (1, 'NFL', '345 Park Avenue New York', 'Jeff Crandall', 'jrc2h@virginia.edu', '434-296-7288'),
 (2, 'ARESIA Inc', '3498-3414 13th St NW Washington', 'Alain Thevenot', 'at@alesia.net', '+12124567890');
 
-/* -- -----------------------------------------------------
+-- -----------------------------------------------------
 -- Insertion dans instance_affectation
 -- -----------------------------------------------------
 INSERT INTO NesterManDB.instance_affectation (id_client, id_instance)
 VALUES 
 (1, 1),
 (1, 2),
-(2, 2); */
+(2, 2);
