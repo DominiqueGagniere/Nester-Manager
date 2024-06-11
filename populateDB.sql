7aeda04bda24554ec221fdaec5492809f1fd8fe6
Dominique Gagnière <dominique@gagnie.re>
2024-06-11 19:10:41 +0200

    update

--- populateDB.sql
+++ populateDB.sql
@@ -4,7 +4,7 @@ DECLARE
     i INT;
 BEGIN
     FOR i IN 1..10 LOOP
-        INSERT INTO "NesterManDB"."harvester" 
+        INSERT INTO NesterManDB.harvester 
             (hostname, mac_addr, ip_lan, ip_wan, serialnum, cpu_name, ram_size_gb, disk_type, disk_size_gb, os_version, harvester_version, id_instance)
         VALUES
             (format('host_harvester_%s', i), format('00:1A:2B:3C:4D:%02s', i), format('192.168.0.%s', i), format('10.0.0.%s', i), i, 'Intel i7', 16, 'SSD', 512, 'Ubuntu 20.04', '1.0', i);
