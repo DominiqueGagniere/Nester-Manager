1b6e32e49a85d6669f16a082020ef8bb8d8f6288
Dominique Gagnière <dominique@gagnie.re>
2024-06-12 18:25:46 +0200

    fix# Please enter the commit message for your changes. Lines starting

--- populateDB.sql
+++ populateDB.sql
@@ -91,6 +91,6 @@ VALUES
 
 INSERT INTO NesterManDB.intervention (id_instance, type, status, id_tech, id_installer, reason)
 VALUES
-(7, 'Maintenance', 'Postponed', 2, 1, 'No documentation for the error, need to contact the developers')
+(7, 'Maintenance', 'Postponed', 2, 1, 'No documentation for the error, need to contact the developers'),
 (20, 'Installation', 'Done', 1, 2, 'Installation successfully done')
 
